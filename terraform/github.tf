1cb00e606267ab956631da2cd16b1a102d572c1c
Fernando Partida <ferpart@hotmail.com>
2021-01-24 17:13:31 -0600

    updated terraform file to add kube config data to secrets

--- terraform/github.tf
+++ terraform/github.tf
@@ -1,5 +1,6 @@
 variable "DOCKERHUB_USERNAME" {}
 variable "DOCKERHUB_TOKEN" {}
+variable "KUBE_CONFIG_DATA" {}
 
 resource "github_actions_secret" "DOCKERHUB_USERNAME" {
     repository= "pokerest"
@@ -11,4 +12,10 @@ resource "github_actions_secret" "DOCKERHUB_TOKEN" {
     repository= "pokerest"
     secret_name = "DOCKERHUB_TOKEN"
     plaintext_value = var.DOCKERHUB_TOKEN
+}
+
+resource "github_actions_secret" "KUBE_CONFIG_DATA" {
+    repository= "pokerest"
+    secret_name = "KUBE_CONFIG_DATA"
+    plaintext_value = var.KUBE_CONFIG_DATA
 }
