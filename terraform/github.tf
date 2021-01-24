5f10193c383fe0fcbe1462bd56a8b15f49358322
Fernando Partida <ferpart@hotmail.com>
2021-01-24 17:41:51 -0600

    updated terraform file to add env keys for aws

--- terraform/github.tf
+++ terraform/github.tf
@@ -1,5 +1,8 @@
 variable "DOCKERHUB_USERNAME" {}
 variable "DOCKERHUB_TOKEN" {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_DEFAULT_REGION" {}
 variable "KUBE_CONFIG_DATA" {}
 
 resource "github_actions_secret" "DOCKERHUB_USERNAME" {
@@ -12,6 +15,24 @@ resource "github_actions_secret" "DOCKERHUB_TOKEN" {
     repository= "pokerest"
     secret_name = "DOCKERHUB_TOKEN"
     plaintext_value = var.DOCKERHUB_TOKEN
+}
+
+resource "github_actions_secret" "AWS_ACCESS_KEY_ID" {
+    repository= "pokerest"
+    secret_name = "AWS_ACCESS_KEY_ID"
+    plaintext_value = var.AWS_ACCESS_KEY_ID
+}
+
+resource "github_actions_secret" "AWS_SECRET_KEY" {
+    repository= "pokerest"
+    secret_name = "AWS_SECRET_KEY"
+    plaintext_value = var.AWS_SECRET_KEY
+}
+
+resource "github_actions_secret" "AWS_DEFAULT_REGION" {
+    repository= "pokerest"
+    secret_name = "AWS_DEFAULT_REGION"
+    plaintext_value = var.AWS_DEFAULT_REGION
 }
 
 resource "github_actions_secret" "KUBE_CONFIG_DATA" {
