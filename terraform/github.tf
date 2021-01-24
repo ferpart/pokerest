65a13dc9f3dcd5a567da8bf0cde4766201429e36
Fernando Partida <ferpart@hotmail.com>
2021-01-24 14:26:38 -0600

    updated github terraform file

--- terraform/github.tf
+++ terraform/github.tf
@@ -4,11 +4,11 @@ variable "DOCKERHUB_TOKEN" {}
 resource "github_actions_secret" "DOCKERHUB_USERNAME" {
     repository= "pokerest"
     secret_name = "DOCKERHUB_USERNAME"
-    plaintext_value = "var.DOCKERHUB_USERNAME"
+    plaintext_value = var.DOCKERHUB_USERNAME
 }
 
 resource "github_actions_secret" "DOCKERHUB_TOKEN" {
     repository= "pokerest"
     secret_name = "DOCKERHUB_TOKEN"
-    plaintext_value = "var.DOCKERHUB_TOKEN"
+    plaintext_value = var.DOCKERHUB_TOKEN
 }
